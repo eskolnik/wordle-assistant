9bdcf6b5d57e3bb0810de393ea61961c2a893a14
Ezra Skolnik <ezraskolnik@gmail.com>
2023-08-03 11:32:00 -0400

    remove all database / activerecord references

--- app/models/data_loader.rb
+++ app/models/data_loader.rb
@@ -13,6 +13,6 @@ class DataLoader
 
     def update_list
         last = source_document.css('.article_body_content ul li a')[0].inner_html
-        WordleSolution.create(word: last, solution_date: Date.yesterday)
+        # WordleSolution.create(word: last, solution_date: Date.yesterday)
     end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,29 @@
 require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+# require "rails"
+
+# active_record is what we're not going to use it, so add it comment in case 
+# at some point you enable it back again
+# require "active_record/railtie" 
+
+# This is not loaded in rails/all but inside active_record so add it if
+# you want your models work as expected
+require "active_model/railtie" 
+# And now the rest
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie" # Only for Rails >= 4.2
+require "action_cable/engine" # Only for Rails >= 5.0
+# require "sprockets/railtie" # Deprecated for Rails >= 7, so add it only if you're using it
+require "rails/test_unit/railtie"
+
+# All these depend on active_record, so they should be excluded also
+# require "action_text/engine" # Only for Rails >= 6.0
+# require "action_mailbox/engine" # Only for Rails >= 6.0
+# require "active_storage/engine" # Only for Rails >= 5.2
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
